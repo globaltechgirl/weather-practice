96ed4bb83b29c642dc219d8345e41cb721f15bcf
onyinyeofili <106868108+onyinyeofili@users.noreply.github.com>
2024-01-28 07:18:06 +0100

    Added my api key

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+function searchCity(city) {
+    let apiKey = "a4o1070de39f538e19a5b7935a4at96f";
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#search-form");
